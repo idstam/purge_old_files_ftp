04a863b44b55054ae027d64bff73c69bfd53e1e8
Johan Idstam <johan@idstam.com>
2022-08-12 15:38:10 +0200

    WIP

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Delete old files from FTP folder.
 
 Trigger delete by count, number of days or accumulated size.
+
+Files written the same minute as the last one to save will also be saved.
--- purge.py
+++ purge.py
@@ -1,14 +1,67 @@
-dir_lines = []
-
+import sys
 from ftplib import FTP
 
-ftp = FTP('ftp.us.debian.org')  # connect to host, default port
-ftp.login()  # user anonymous, passwd anonymous@
-ftp.cwd('debian')  # change into "debian" directory
-ftp.retrlines('LIST', )  # list directory contents
+from dateutil import parser
 
-ftp.quit()
+file_infos = []
 
 
 def collect_files(dir_line):
-    dir_lines.append(dir_line)
+    file_info = {}
+    tokens = dir_line.split()
+    file_info['size'] = int(tokens[4])
+    file_info['ftp_date'] = tokens[5] + " " + tokens[6] + " " + tokens[7]
+    file_info['file_name'] = tokens[8]
+    ftp_date = parser.parse(file_info['ftp_date'])
+    file_info['sortable_date'] = ftp_date.strftime("%Y-%m-%d %H:%M")
+
+    if file_info['file_name'] == "." or file_info['file_name'] == "..":
+        return
+
+    file_infos.append(file_info)
+
+
+def ftp_date_key(file_info):
+    return file_info['sortable_date']
+
+
+user = sys.argv[1]
+pwd = sys.argv[2]
+server = sys.argv[3]
+folder = sys.argv[4]
+
+# c = number of files to keep, s = megabytes to keep, d = days to keep
+trigger_type = sys.argv[5]
+trigger_value = int(sys.argv[6])
+
+print("Server: " + server)
+
+ftp = FTP(server)
+ftp.login(user, pwd)
+ftp.cwd(folder)  # change into "debian" directory
+ftp.retrlines('LIST', collect_files)  # list directory contents
+
+file_infos.sort(key=ftp_date_key, reverse=True)
+
+count = 0
+acc_size = 0
+last_date = ''
+
+for fi in file_infos:
+    count = count + 1
+    acc_size = acc_size + fi['size']
+    trigger_size = acc_size / (1024 * 1024)
+
+    if trigger_type == "c" and count > trigger_value and fi['sortable_date'] != last_date:
+        print("Would delete due to count")
+
+    if trigger_type == "s" and trigger_size > trigger_value and fi['sortable_date'] != last_date:
+        print("Would delete due to size")
+
+    #    if trigger_type == "d" and trigger_value > count:
+    #        print("Would delete due to date")
+
+    last_date = fi['sortable_date']
+    print(fi['sortable_date'] + " " + fi['file_name'] + " " + str(trigger_size))
+
+ftp.quit()
